dea1ec6b4202f911697f3793060eb3f7b32d939d
VorotyaginAnton <a.vorotyagin@yandex.ru>
2021-11-18 20:23:12 +0300

    refactor hw03

--- hw03.js
+++ hw03.js
@@ -5,21 +5,20 @@ const ip1 = '89.123.1.41';
 const ip2 = '34.48.240.111';
 
 function hw03() {
+    const writeStreamIp1 = fs.createWriteStream(`./${ip1}_requests.log`, { flags: 'a', encoding: 'utf8' });
+    const writeStreamIp2 = fs.createWriteStream(`./${ip2}_requests.log`, { flags: 'a', encoding: 'utf8' });
+
     let s = fs.createReadStream('./access.log', 'utf8')
         .pipe(es.split())
         .pipe(es.mapSync(function(line){
-            s.pause();
-
             if (line.match(new RegExp(`^${ip1}`))) {
-                writeToLog(line, 1);
+                writeStreamIp1.write(line+'\n');
                 console.log(line);
             }
             if (line.match(new RegExp(`^${ip2}`))) {
-                writeToLog('test'+line, 2);
+                writeStreamIp2.write(line+'\n');
                 console.log(line);
             }
-
-            s.resume();
         }))
         .on('error', function(err){
             console.log('Error while reading file.', err);
@@ -27,21 +26,6 @@ function hw03() {
         .on('end', function(){
             console.log('Read entire file.')
         });
-}
-
-function writeToLog (row, fileType) {
-    let fileName = '';
-    if (fileType === 1) {
-        fileName = `./${ip1}_requests.log`;
-    }
-    else if (fileType === 2) {
-        fileName = `./${ip2}_requests.log`;
-    }
-
-    const writeStream = fs.createWriteStream(fileName, { flags: 'a', encoding: 'utf8' });
-    writeStream.write(row+'\n');
-
-    writeStream.end(() => console.log(`${fileName} has been updated`));
 }
 
 module.exports = hw03;
